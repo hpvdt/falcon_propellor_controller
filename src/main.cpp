f0efd1933d7d53ca9cd48b26d9f43f0d70d7f36e
Claire Nolet <claire.nolet@gmail.com>
2024-02-10 16:54:06 -0500

    Servo1 and servo2 connections test (successful)

--- src/main.cpp
+++ src/main.cpp
@@ -23,4 +23,15 @@ void loop() {
   delay(200);
   digitalWrite(pinLED3, LOW);
   delay(200);
+
+  // Servo
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                            // waits for the servo to get there
+  }
 }
